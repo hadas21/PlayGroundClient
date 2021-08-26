adc23c75e2c486d2f5ff31e3cbb2db167c1c9b00
hernandoit <hernandohernandezit@gmail.com>
2021-08-25 20:29:58 -0400

    add friend api calls
    add friend.js api calls in the api folder in src folder

---
+++ src/api/friend.js
@@ -0,0 +1,46 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createFriend = (data, user) => {
+  console.log(data, user)
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/friends',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: {
+      location: data
+    }
+  })
+}
+
+export const indexFriends = (user) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/friends',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+export const showFriend = (id, user) => {
+  return axios({
+    method: 'GET',
+    url: `${apiUrl}/friends/${id}`,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
+
+export const deleteFriend = (id, user) => {
+  return axios({
+    url: apiUrl + '/friends/' + id,
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
