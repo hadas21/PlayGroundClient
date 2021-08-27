1b5a2206e51fb57e2ba6036313b9baead1575460
hadas21 <hadas.axel@gmail.com>
2021-08-27 13:18:29 -0500

    resolve conflict

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -5,6 +5,8 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 import './../../index.scss'
 import { indexLocations } from '../../api/location'
 import { saveLocation } from '../../api/map'
+// import CreateLocation from '../../components/location/CreateLocation'
+// import AuthenticatedRoute from '../../components/AuthenticatedRoute/AuthenticatedRoute'
 import Sidebar from './Sidebar'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibGF1cmFhbHlzb24iLCJhIjoiY2tzcDJleWVkMDF0NjMxcGhwMzM1Mm1tMiJ9.27PwqNrg2-gZnMmuS1vOww'
@@ -16,84 +18,96 @@ class Map extends Component {
     this.state = {
       lng: '',
       lat: '',
-      zoom: '',
-      address: ''
+      zoom: ''
     }
     this.mapContainer = React.createRef()
   }
 
-  componentDidMount () {
-  // const { zoom } = this.state
-    const map = new mapboxgl.Map({
-      container: this.mapContainer.current,
-      style: 'mapbox://styles/lauraalyson/cksp2t5nr6w2m17o33s38ftds',
-      center: [-13, 34],
-      zoom: 2
-    })
+address = ''
 
-    indexLocations(this.props.user)
-    // .then((res) => console.log(res))
-      .then((res) => {
-        for (const { coordinates } of res.data.locations) {
+componentDidMount () {
+  // const { zoom } = this.state
+  const map = new mapboxgl.Map({
+    container: this.mapContainer.current,
+    style: 'mapbox://styles/lauraalyson/cksp2t5nr6w2m17o33s38ftds',
+    center: [-13, 34],
+    zoom: 2
+  })
+
+  indexLocations(this.props.user)
+  // .then((res) => console.log(res))
+    .then((res) => {
+      for (const { coordinates } of res.data.locations) {
         // make a marker for each feature and add to the map
-          new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
-        }
+        new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+      }
+    })
+    .catch((err) => console.log(err))
+  // map.dragRotate.enable()
+
+  // On click function
+  map.on('load', () => {
+    // console.log(e)
+    // this.setState({
+    //   lng: e.lngLat.lng,
+    //   lat: e.lngLat.lat,
+    //   zoom: map.getZoom().toFixed(2)
+    // })
+
+    saveLocation(this.state.lng, this.state.lat)
+      .then((res) => {
+        console.log(res.data)
+        this.address = res.data.features[1].place_name
+        console.log('This is this.address 1', this.address)
       })
       .catch((err) => console.log(err))
 
-    // On click function
-    map.on('click', (e) => {
+    const marker = new mapboxgl.Marker({ draggable: true })
+      .setLngLat({ lng: this.state.lng, lat: this.state.lat })
+      .addTo(map)
+    console.log('this is marker: ', marker)
+    function onDragEnd (e) {
       console.log(e)
-      this.setState({
-        lng: e.lngLat.lng,
-        lat: e.lngLat.lat,
-        zoom: map.getZoom().toFixed(2)
-      }
-      )
-
-      saveLocation(this.state.lng, this.state.lat)
-        .then((res) => {
-          console.log(res.data)
-          this.setState({
-            address: res.data.features[1].place_name
-          })
-        })
-        .catch((err) => console.log(err))
-
-      const marker = new mapboxgl.Marker()
-        .setLngLat({ lng: this.state.lng, lat: this.state.lat })
-        .addTo(map)
-      console.log('this is marker: ', marker)
+      // this.setState({
+      //   lng: e.lngLat.lng,
+      //   lat: e.lngLat.lat,
+      //   zoom: map.getZoom().toFixed(2)
+      // })
+      // const lngLat = marker.getLngLat()
+      // coordinates.style.display = 'block'
+      // coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`
     }
-    )
 
-    const geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken
+    marker.on('dragend', onDragEnd)
+  })
 
-    })
-    map.addControl(geocoder)
-  }
+  const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
 
-  render () {
-    const { lng, lat, zoom, address } = this.state
-    const { user, msgAlert } = this.props
-    return (
-      <div>
-        <Sidebar
-          lng={lng}
-          lat={lat}
-          msgAlert={msgAlert}
-          user={user}
-          address={address}
-          map={this.map}
-        />
-        <div ref={this.mapContainer} className='map-container' />
-        <div className='lat-long'>Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      This is address: {address}
-        </div>
+  })
+  map.addControl(geocoder)
+}
+
+render () {
+  const { lng, lat, zoom } = this.state
+  const { user, msgAlert } = this.props
+  return (
+    <div>
+      <Sidebar>
+        lng={lng}
+        lat={lat}
+        msgAlert={msgAlert}
+        user={user}
+        address={this.address}
+        map={this.map}
+      </Sidebar>
+      <div ref={this.mapContainer} className='map-container' />
+      <div className='lat-long'>Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      This is address: {this.address}
       </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
 export default Map
