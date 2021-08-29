2871678eebce90519e6880044c10813f067b4521
hadas21 <hadas.axel@gmail.com>
2021-08-29 15:22:42 -0500

    resolve

--- src/components/auth/Users.js
+++ src/components/auth/Users.js
@@ -23,44 +23,35 @@ class Users extends Component {
     // make a GET request for all of the books
     axios(`${apiUrl}/users`)
       .then((res) => {
-<<<<<<< HEAD
         console.log(res.data.user)
         const response = res.data.user.map((user) => user.username)
         this.setState({ users: response })
-=======
-        console.log(res)
-        this.setState({ users: res.data.users })
->>>>>>> fc0cb16 (g)
       })
       .catch(console.error)
   }
 
   render () {
-<<<<<<< HEAD
-    // const users = this.state
-=======
     const users = this.state.user
     // .map(user => (
     //   <li key={user._id}>
     //     <Link to={`/users/${user._id}`}>{user.username}</Link>
     //   </li>
     // ))
->>>>>>> fc0cb16 (g)
+
+    // const users = this.state
 
     return (
       <>
         <h4>Users</h4>
-<<<<<<< HEAD
-        {/* <ul>
+
+        <ul>
           {users.map((user) => (
             <li key={user._id}>
               {user}
             </li>
           ))}
-        </ul> */}
-=======
-        <ul>{users}</ul>
->>>>>>> fc0cb16 (g)
+        </ul>
+
       </>
     )
   }
